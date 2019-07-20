f07041f58dc973f0e40009c390c337b00f5b1ed3
giancarlo <gsavoryalves@gmail.com>
2019-07-20 15:58:37 -0400

    finish program

---
+++ script.js
@@ -0,0 +1,14 @@
+function isEven (n){
+    return n % 2 === 0;
+}       
+
+const input = document.getElementById('number-input');
+const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+
+
+btn.onclick = () => {
+    
+    const num  = Number(input.value);
+    result.textContent = `${num} is ${isEven(num) ? "even" : "odd"}`;
+}
